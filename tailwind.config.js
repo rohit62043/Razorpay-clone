eba5f8178a72345e06c7ebe8252fc3b9fffe4a58
Rohit <singhrohit51967@gmail.com>
2023-07-12 20:36:07 +0530

    Creating Nav Bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mullish: ["mullish", "sans-serif"],
+      },
+      colors: {
+        deepBlue: "#02042a",
+        lightBlue: "#2b84ead",
+        lightBlue300: "#4b94ed",
+        lightBlue500: "#0b72e7",
+        greenLight: "#61cea6",
+        grayBlue: "#344a6c",
+        lightGray: "#e2e2e2",
+        grayText: "#818597",
+        deepBlueHead: "#162f56",
+        gray2: "#525a76",
+      }
+    },
   },
   plugins: [],
 }
